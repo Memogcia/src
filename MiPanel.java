ffb092d1b859047e6af76bf4dfb9bc36430a9c28
Guillermo Garcia Contreras <newwilliam94@gmail.com>
2014-03-24 20:34:56 -0600

    Begining

---
+++ MiPanel.java
@@ -0,0 +1,13 @@
+//A01228901 VICTOR RENE DOMINGUEZ SUAREZ
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+
+public class MiPanel extends JPanel{
+	
+	public MiPanel(){
+		this.setPreferredSize(new Dimension(1024,768));
+	}
+}
